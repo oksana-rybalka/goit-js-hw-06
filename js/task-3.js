cd69661720d9632aaa542cc605585baecc01560d
oksana-rybalka <rybalka-oksana@outlook.com>
2024-09-28 20:20:31 +0300

    task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,32 @@
 class StringBuilder {
-    #value
+    #value;
+    constructor(initialValue) {
+        this.#value = initialValue;
+        
+    }
+    getValue() {
+        return this.#value;
+    }
+    padEnd(str) {
+        this.#value = this.#value + str;
+        return this.#value;
+    }
+    padStart(str) {
+        this.#value = str + this.#value;
+        return this.#value
+    }
+    padBoth(str) {
+        this.#value = str + this.#value + str;
+        return this.#value;
 }
+}
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
